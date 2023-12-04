76c4cf6761baa6db64ee529848062ebfbf195a40
mishaniahomi <misha.homi@gmail.com>
2023-12-04 15:34:05 +0300

    Добавляем карту сайта

--- apkaba/settings.py
+++ apkaba/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'search',
     'rest_framework',
     'django_elasticsearch_dsl',
+    'django.contrib.sites', 
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
@@ -186,3 +188,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 25
 }
+
+SITE_ID = 1 
--- apkaba/urls.py
+++ apkaba/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from main.sitemap import PostSitemap
 
+sitemaps = {  
+    'posts': PostSitemap,  
+}  
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path("ckeditor/", include('ckeditor_uploader.urls')),
     path('search/', include('search.urls')),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap",),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
