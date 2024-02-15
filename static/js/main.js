0b91e7a8e5fb6d47e320c1d1453390d32dc7cadd
mishaniahomi <69090763+mishaniahomi@users.noreply.github.com>
2024-02-15 12:16:35 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -35,44 +35,44 @@ function activate_photo(id) {
 function get_posts(query){
     if (query){
         let url = "/search/posts/"+query;
-        $.get(url, function(data, status){
-            if(data.length){
-                let elem = document.getElementById("news_search");
-                data.forEach(function(item, index, array){
-                    let clone = tmpl.content.cloneNode(true);
-                        let h5 = clone.querySelectorAll("h5");
-                        h5[0].textContent = item['title'];
-                        let a = clone.querySelectorAll("a");
-                        a[0].href ='/news/'+item['slug'];
-                        let p = clone.querySelectorAll("p");
-                        p[0].textContent = item['created_at'];
-                        let img = clone.querySelectorAll("img");
-                        img[0].src = item['picture_url'];
-                        elem.append(clone);
-           });
-            }
-        });
-        // получение от es данных по поиску о важной информации
-        url = "/search/importantinfo/"+query;
-        $.get(url, function(data, status){
+        // $.get(url, function(data, status){
+        //     if(data.length){
+        //         let elem = document.getElementById("news_search");
+        //         data.forEach(function(item, index, array){
+        //             let clone = tmpl.content.cloneNode(true);
+        //                 let h5 = clone.querySelectorAll("h5");
+        //                 h5[0].textContent = item['title'];
+        //                 let a = clone.querySelectorAll("a");
+        //                 a[0].href ='/news/'+item['slug'];
+        //                 let p = clone.querySelectorAll("p");
+        //                 p[0].textContent = item['created_at'];
+        //                 let img = clone.querySelectorAll("img");
+        //                 img[0].src = item['picture_url'];
+        //                 elem.append(clone);
+        //    });
+        //     }
+        // });
+        // // получение от es данных по поиску о важной информации
+//         url = "/search/importantinfo/"+query;
+//         $.get(url, function(data, status){
 
-            if(data.length){
-                let elem = document.getElementById("news_search");
-                data.forEach(function(item, index, array){
-//                    console.log(data);
-                    let clone = tmpl.content.cloneNode(true);
-                        let h5 = clone.querySelectorAll("h5");
-                        h5[0].textContent = item['title'];
-                        let a = clone.querySelectorAll("a");
-                        a[0].href ='/news/'+item['slug'];
-                        let p = clone.querySelectorAll("p");
-                        p[0].textContent = item['created_at'];
-                        let img = clone.querySelectorAll("img");
-                        img[0].src = item['picture_url'];
-                        elem.append(clone);
-           });
-            }
-        });
+//             if(data.length){
+//                 let elem = document.getElementById("news_search");
+//                 data.forEach(function(item, index, array){
+// //                    console.log(data);
+//                     let clone = tmpl.content.cloneNode(true);
+//                         let h5 = clone.querySelectorAll("h5");
+//                         h5[0].textContent = item['title'];
+//                         let a = clone.querySelectorAll("a");
+//                         a[0].href ='/news/'+item['slug'];
+//                         let p = clone.querySelectorAll("p");
+//                         p[0].textContent = item['created_at'];
+//                         let img = clone.querySelectorAll("img");
+//                         img[0].src = item['picture_url'];
+//                         elem.append(clone);
+//            });
+//             }
+//         });
         // получение от es данных по поиску о Техники
         url = "/search/machine/"+query;
         $.get(url, function(data, status){
@@ -136,44 +136,44 @@ function get_posts(query){
         });
 
         // получение от es данных по поиску о альбомах
-        url = "/search/albom/"+query;
-        $.get(url, function(data, status){
+        // url = "/search/albom/"+query;
+        // $.get(url, function(data, status){
          
-            if(data.length){
-                let elem = document.getElementById("news_search");
-                data.forEach(function(item, index, array){
-                    let clone = tmpl.content.cloneNode(true);
-                        let h5 = clone.querySelectorAll("h5");
-                        h5[0].textContent = item['name'];
-                        let a = clone.querySelectorAll("a");
-                        a[0].href ='/albom/'+item['slug'];
-                        let p = clone.querySelectorAll("p");
-                        p[0].textContent = item['date'];
-                        let img = clone.querySelectorAll("img");
-                        img[0].src = item['picture_url'];
-                        elem.append(clone);
-           });
-            }
-        });
+        //     if(data.length){
+        //         let elem = document.getElementById("news_search");
+        //         data.forEach(function(item, index, array){
+        //             let clone = tmpl.content.cloneNode(true);
+        //                 let h5 = clone.querySelectorAll("h5");
+        //                 h5[0].textContent = item['name'];
+        //                 let a = clone.querySelectorAll("a");
+        //                 a[0].href ='/albom/'+item['slug'];
+        //                 let p = clone.querySelectorAll("p");
+        //                 p[0].textContent = item['date'];
+        //                 let img = clone.querySelectorAll("img");
+        //                 img[0].src = item['picture_url'];
+        //                 elem.append(clone);
+        //    });
+        //     }
+        // });
 
          // получение от es данных по поиску о Баннерах
-         url = "/search/banneraakcii/"+query;
-         $.get(url, function(data, status){
+         // url = "/search/banneraakcii/"+query;
+         // $.get(url, function(data, status){
             
-             if(data.length){
-                 let elem = document.getElementById("news_search");
-                 data.forEach(function(item, index, array){
-                     let clone = tmpl.content.cloneNode(true);
-                         let h5 = clone.querySelectorAll("h5");
-                         h5[0].textContent = item['title'];
-                         let a = clone.querySelectorAll("a");
-                         a[0].href ='/'+item['href'];
-                         let img = clone.querySelectorAll("img");
-                         img[0].src = item['picture_url'];
-                         elem.append(clone);
-            });
-             }
-         });
+         //     if(data.length){
+         //         let elem = document.getElementById("news_search");
+         //         data.forEach(function(item, index, array){
+         //             let clone = tmpl.content.cloneNode(true);
+         //                 let h5 = clone.querySelectorAll("h5");
+         //                 h5[0].textContent = item['title'];
+         //                 let a = clone.querySelectorAll("a");
+         //                 a[0].href ='/'+item['href'];
+         //                 let img = clone.querySelectorAll("img");
+         //                 img[0].src = item['picture_url'];
+         //                 elem.append(clone);
+         //    });
+         //     }
+         // });
         
          // получение от es данных по поиску о Категориях
          url = "/search/categories/"+query;
@@ -194,33 +194,33 @@ function get_posts(query){
             });
              }
          });
-        // получение от es данных по поиску о Контенте страниц
-        url = "/search/pagecontent/"+query;
-        $.get(url, function(data, status){
+        // // получение от es данных по поиску о Контенте страниц
+        // url = "/search/pagecontent/"+query;
+        // $.get(url, function(data, status){
             
-            if(data.length){
-                let elem = document.getElementById("news_search");
-                data.forEach(function(item, index, array){
-                    let clone = tmpl.content.cloneNode(true);
-                        let h5 = clone.querySelectorAll("h5");
-                        h5[0].textContent = item['title'];
-                        let a = clone.querySelectorAll("a");
-                        if(item['href']){
-                            a[0].href ='/'+item['href'];
-                        }
-                        else{
-                            a[0].href ='/content/'+item['slug'];
-                        }
+        //     if(data.length){
+        //         let elem = document.getElementById("news_search");
+        //         data.forEach(function(item, index, array){
+        //             let clone = tmpl.content.cloneNode(true);
+        //                 let h5 = clone.querySelectorAll("h5");
+        //                 h5[0].textContent = item['title'];
+        //                 let a = clone.querySelectorAll("a");
+        //                 if(item['href']){
+        //                     a[0].href ='/'+item['href'];
+        //                 }
+        //                 else{
+        //                     a[0].href ='/content/'+item['slug'];
+        //                 }
                         
                         
-                        let p = clone.querySelectorAll("p");
-                        p[0].textContent = item['created_at'];
-                        let img = clone.querySelectorAll("img");
-                        img[0].src = item['picture_url'];
-                        elem.append(clone);
-           });
-            }
-        });
+        //                 let p = clone.querySelectorAll("p");
+        //                 p[0].textContent = item['created_at'];
+        //                 let img = clone.querySelectorAll("img");
+        //                 img[0].src = item['picture_url'];
+        //                 elem.append(clone);
+        //    });
+        //     }
+        // });
     
        
 
